6a1ccfed8fd74c0fd6051c2343564621705b5cde
Jason Bracco <bracco.jason@gmail.com>
2022-06-07 19:55:25 -0400

    final html setup

---
+++ index.js
@@ -0,0 +1,17 @@
+{/* <div id="player-section">
+<h2>NBA Player:</h2>
+<form id="player-search">
+    <input id="search" type="text" name="search">
+    <input type="submit" name="submit">
+</form>
+</div>
+<div id="team-selection">
+ <h2>NBA Team:</h2>
+<form id="team-search">
+    <input id="search" type="text" name="search">
+    <input type="submit" name="submit">
+</form> */}
+
+function getButtons{
+    console.log(document.getElementById('team-button'))
+}
