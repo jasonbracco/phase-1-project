224ccc8cf2172ff98fa4f7535b5cb66023166172
Jason Bracco <bracco.jason@gmail.com>
2022-06-08 19:34:41 -0400

    grabbing and displaying player first and last name

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ function handleTeamSubmit(event){
     console.log(teamInputText)
 }
 
-
+function fetchPlayer(playerInputText) {
+    fetch(`https://www.balldontlie.io/api/v1/players?search=${playerInputText}`)
+    .then(response => response.json())
+    .then(playerData => {
+        console.log(playerData.data[0].first_name)
+        console.log(playerData.data[0].last_name)
+    })
+}
 
 
