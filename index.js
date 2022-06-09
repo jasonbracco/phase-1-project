54b839caeb9e5f79428352064e8750ede1e3e35a
Jason Bracco <bracco.jason@gmail.com>
2022-06-08 20:43:33 -0400

    displaying the players that match the search

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function handleTeamSubmit(event){
 function fetchPlayer(playerInputText) {
     fetch(`https://www.balldontlie.io/api/v1/players?search=${playerInputText}&per_page=100`)
     .then(response => response.json())
-    .then(playerData => {playerData.data.forEach(player => renderPlayer(player))
+    .then(playerData => {playerData.data.forEach(player => renderPlayer(player)) //each one is an object
     })
 }
 
@@ -30,6 +30,15 @@ function fetchTeam(teamInputText) {
     })
 }
 
-
+function renderPlayer(player){
+    let playerList = document.getElementById('player-list')
+    newPlayer = document.createElement('li')
+    newPlayer.className = 'player'
+    newPlayer.id = `${player.id}`
+    newPlayer.innerHTML = `
+    ${player.first_name} ${player.last_name}
+    `
+    playerList.appendChild(newPlayer)
+}
 
 
