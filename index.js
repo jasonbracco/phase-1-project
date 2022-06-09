3207f22632f20c03fd38ef5e0384dc5999ae4dd9
Jason Bracco <bracco.jason@gmail.com>
2022-06-08 22:27:53 -0400

    displays all teams matching search criteria

--- index.js
+++ index.js
@@ -24,9 +24,14 @@ function fetchPlayer(playerInputText) {
 }
 
 function fetchTeam(teamInputText) {
-    fetch(`https://www.balldontlie.io/api/v1/teams?search${teamInputText}`)
+    fetch(`https://www.balldontlie.io/api/v1/teams?search`)
     .then(response => response.json())
-    .then(teamData => {teamData.data.forEach(team => renderTeam(team))
+    .then(teamData => {
+        let teamArray = [] //this API does not allow a search by team
+        let teamInformation = teamData.data
+        teamData.data.forEach(object =>teamArray.push(object.full_name.toUpperCase()))
+        renderTeam(teamInputText,teamInformation, teamArray)
+        console.log(teamArray)
     })
 }
 
@@ -35,10 +40,35 @@ function renderPlayer(player){
     newPlayer = document.createElement('li')
     newPlayer.className = 'player'
     newPlayer.id = `${player.id}`
-    newPlayer.innerHTML = `
-    ${player.first_name} ${player.last_name}
-    `
+    newPlayer.innerHTML = `${player.first_name} ${player.last_name}`
     playerList.appendChild(newPlayer)
 }
+
+
+function renderTeam(teamInputText, teamInformation, teamArray){
+    //take the input text and run it through the array
+    //if it matches anything in the array, create a list element with that team
+    console.log(teamInformation)
+    let transformedTeamText = teamInputText.toUpperCase()
+    let requestedTeam = teamArray.filter(team => team.includes(transformedTeamText))
+    console.log(requestedTeam)
+    if(requestedTeam.length !==0){
+        requestedTeam.forEach(element =>{
+        let teamList = document.getElementById('team-list')
+        newTeam = document.createElement('li')
+        newTeam.className = 'team'
+        newTeam.innerHTML = `${element}`
+        teamList.appendChild(newTeam)
+        })
+    }
+    else{
+
+    }
+}
+
+
+
+
+
 
 
